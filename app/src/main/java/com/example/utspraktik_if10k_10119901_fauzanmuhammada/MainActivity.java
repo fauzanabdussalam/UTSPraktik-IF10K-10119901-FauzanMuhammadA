1f34f0d97eac479e74e9b7555fe76a9ef9129a81
fauzan_abdussalam <fauzan@3trust.com>
2022-06-03 20:41:27 +0700

    init

---
+++ app/src/main/java/com/example/utspraktik_if10k_10119901_fauzanmuhammada/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.utspraktik_if10k_10119901_fauzanmuhammada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
